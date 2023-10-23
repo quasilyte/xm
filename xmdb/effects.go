edc12599eca36497b1cd660ae1abe1d695faaa38
Iskander Sharipov <quasilyte@gmail.com>
2023-10-23 18:54:34 +0400

    add a helper xmdb package

---
+++ xmdb/effects.go
@@ -0,0 +1,53 @@
+package xmdb
+
+import "github.com/quasilyte/xm/xmfile"
+
+type Effect struct {
+	Op  EffectOp
+	Arg uint8
+}
+
+type EffectOp int
+
+const (
+	EffectNone EffectOp = iota
+
+	// Encoding: effect=0x0C [or] volume byte
+	// Arg: volume level
+	EffectSetVolume
+
+	// Encoding: effect=0x14 [or] key-off note
+	// Arg: tick number (always a first tick for key-off note)
+	EffectKeyOff
+)
+
+func ConvertEffect(n xmfile.PatternNote) Effect {
+	e := Effect{Arg: n.EffectParameter}
+
+	switch n.EffectType {
+	case 0x14:
+		e.Op = EffectKeyOff
+	}
+
+	return e
+}
+
+func EffectFromVolumeByte(v uint8) Effect {
+	var e Effect
+
+	switch {
+	case v <= 0x0F:
+		// Do nothing.
+
+	case v <= 0x50:
+		// Set volume effect.
+		e.Op = EffectSetVolume
+		e.Arg = v - 0x10
+	}
+
+	return e
+}
+
+func (e Effect) AsUint16() uint16 {
+	return (uint16(e.Op) << 8) | uint16(e.Arg)
+}
